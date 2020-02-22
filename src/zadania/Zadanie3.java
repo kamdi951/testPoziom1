d21a70e9b8617f028aba5bdaa85e66fa466b6126
kamdi/kamdi951 <rycewiczk@gmail.com>
2020-02-22 15:37:49 +0100

    zad2

--- src/zadania/Zadanie3.java
+++ src/zadania/Zadanie3.java
@@ -3,25 +3,40 @@ package zadania;
 public class Zadanie3 {
 
     public static void main(String[] args) {
+        Węzeł<Integer> e = new Węzeł<>(24, null, null);
+        Węzeł<Integer> d = new Węzeł<>(66, null, null);
+        Węzeł<Integer> c = new Węzeł<>(8, null, null);
+        Węzeł<Integer> b = new Węzeł<>(100, d, e);
+        Węzeł<Integer> a = new Węzeł<>(1, c, null);
+        Węzeł<Integer> korzen = new Węzeł<>(50, a, b);
 
-        //int ile = countLisc();
+        int ile = countLisc(korzen,0);
+        System.out.println(ile);
+
+
     }
 
 
-    public static int countLisc(Węzeł<Integer> węzeł) {
+    public static int countLisc(Węzeł<Integer> węzeł,int ile) {
         if (węzeł == null) {
             return 0;
         }
-        int ile = 0;
 
-        int prawy = countLisc(węzeł.getPrawy());
-        if (węzeł.getLewy() != null || węzeł.getPrawy() != null) {
-            ile++;
+        int prawy = countLisc(węzeł.getPrawy(),ile);
+        if (węzeł.getPrawy() == null) {
+            int lewy = countLisc(węzeł.getLewy(),ile);
+            if (węzeł.getLewy() == null ) {
+                ile++;
         }
 
-        int lewy = countLisc(węzeł.getLewy());
-        if (węzeł.getLewy() != null || węzeł.getPrawy() != null) {
-            ile++;
+
+
+//            ile++;
+//            System.out.println("prawy "+ile);
+//
+//            int lewy = countLisc(węzeł.getLewy(),ile);
+//            ile++;
+//            System.out.println("lewy "+ile);
         }
 
         return ile;
