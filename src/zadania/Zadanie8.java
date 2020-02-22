34d8a0c9944606102d53c9cc7038dac4f6e7ca43
kamdi/kamdi951 <rycewiczk@gmail.com>
2020-02-22 11:42:11 +0100

    zad8

--- src/zadania/Zadanie8.java
+++ src/zadania/Zadanie8.java
@@ -7,22 +7,29 @@ public class Zadanie8 {
         tablica[1] = 2;
         tablica[2] = 3;
         tablica[3] = 4;
-        tablica[4] = -2;
-        tablica[5] = 10;
+        tablica[4] = 6;
+        tablica[5] = 7;
 
         int dlugosc = tablica.length;
         int suma = 0;
+        int sumaWew = 0;
         int ostatni = 0;
+        int[] tablicaSum=new int[dlugosc];
 
         for (int i = 0; i < dlugosc-1; i++) {
 
-            if (tablica[i] + 1 == tablica[i + 1]) {
-                suma += tablica[i];
+            if(tablica[i] + 1 == tablica[i + 1]) {
+
+                sumaWew += tablica[i];
                 ostatni = tablica[i + 1];
                 System.out.println(suma);
+            }else{
+                sumaWew=0;
+            }
+            if(suma <= sumaWew + ostatni){
+                suma = sumaWew + ostatni;
             }
         }
-        suma += ostatni;
         System.out.println(suma);
     }
 }
